62c80025a541ddbb003ec7a62452815c47094667
vattvichet <vichetvattedu@gmail.com>
2021-04-19 17:15:02 +0700

    const for consistent

--- math.js
+++ math.js
@@ -14,3 +14,11 @@ let year = 2003;
 console.log(year + 1)
 console.log(year--)
 console.log(a + year + b)
+let c = 20;
+const d = 30;
+console.log(c /= 2)
+console.log(c *= 2)
+console.log(c += 1)
+console.log(c -= 5)
+console.log(c + 1)
+console.log(c)
