29b4b7e2be89e1a9a81cc5799f78c58aef4df08a
vattvichet <vichetvattedu@gmail.com>
2021-04-30 21:54:04 +0700

    indexOf

--- strings.js
+++ strings.js
@@ -1,5 +1,8 @@
 let fever = "you are definitely joke lololol";
-console.log(fever.replace('are', 'r'));
-console.log(fever.repeat(2));
-console.log(fever.slice(2));
+console.log(fever.replace('are', 'r')); //replace the letter 
+console.log(fever.repeat(2));// repeat more 
+console.log(fever.slice(2));//start from word number
+
+let cold = "we are incredible";
+console.log(cold.indexOf("are"));//number of fisrt letter
 
