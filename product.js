1f352fb32da1531aafab0ea37deb40f6658c64ae
vattvichet <vichetvattedu@gmail.com>
2021-05-02 21:36:54 +0700

    using backtick

---
+++ product.js
@@ -0,0 +1,5 @@
+let product = "Iphone";
+let mode = "second hand"
+let price = "1000";
+let amount = "7";
+console.log(`You have bought ${amount} ${mode.toUpperCase()} ${product} Total Price :  ${price * amount}$ `);
