87bc380c36dbfaeacad82aa8442603c6c205b335
vattvichet <vichetvattedu@gmail.com>
2021-05-02 21:57:19 +0700

    random

--- product.js
+++ product.js
@@ -3,3 +3,12 @@ let mode = "second hand"
 let price = "1000";
 let amount = "7";
 console.log(`You have bought ${amount} ${mode.toUpperCase()} ${product} Total Price :  ${price * amount}$ `);
+//use `` for adding more text without using ++
+
+console.log(Math.PI);
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10));
